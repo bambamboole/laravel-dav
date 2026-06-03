848f52c3b15ec4e8284ae57908691bf9099cbcba
Manuel Christlieb <manuel@christlieb.eu>
2026-06-04 01:27:27 +0200

    feat: auto-load package migrations

--- src/LaravelDavServiceProvider.php
+++ src/LaravelDavServiceProvider.php
@@ -13,6 +13,8 @@ class LaravelDavServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
         $this->publishes([__DIR__.'/../config/dav.php' => config_path('dav.php')], 'dav-config');
         $this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')], 'dav-migrations');
     }
